e5c43056afe0aa295bccb4e67a193be2c07e869f
Clemens Henrich <122051276+CLMNS22@users.noreply.github.com>
2023-12-08 14:59:21 +0100

    freitag nachmittag

--- first_django_app/first_django_app/settings.py
+++ first_django_app/first_django_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'chat',
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'first_django_app.urls'
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Livereload settings
+LIVERELOAD_HOST = 'http://127.0.0.1/'
+LIVERELOAD_PORT = '8000'
