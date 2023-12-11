8eb486fbe13d799716b6f6aeceb818845bc36f7e
Clemens Henrich <122051276+CLMNS22@users.noreply.github.com>
2023-12-11 16:17:05 +0100

    Video 10
    Chatverlauf anzeigen lassen und Nachricht hinzufügen.

--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -11,6 +11,6 @@ class Chat(models.Model):
 class Message(models.Model):
         text = models.CharField(max_length=500)
         created_at = models.DateField(default=date.today)
-        chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=True)
+        chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=True, blank=True, default=None, related_name='chat_message_set')
         author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
         receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,5 +1,24 @@
+# from django.shortcuts import render
+# from .models import Chat, Message
+
+# # Create your views here.
+# def index(request):
+#     if request.method == 'POST':
+#         print('received data' + request.POST['messageText'])
+#         myChat = Chat.objects.get(id=1)
+#         Message.objects.create(text=request.POST['messageText'], chat=myChat, author=request.user, receiver=request.user)
+#         chatMessages = Message.objects.filter(chat__id=1)
+#     return render(request, 'chat/index.html', {"message": chatMessages})
+
 from django.shortcuts import render
+from .models import Chat, Message
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html', {'username': 'Clemens'})
+    if request.method == 'POST':
+        print('received data' + request.POST['messageText'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['messageText'], chat=myChat, author=request.user, receiver=request.user)
+    
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {"messages": chatMessages})
